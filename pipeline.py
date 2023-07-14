842c95aa680755648ec41e6714d6d2ad01403eaa
adaFetic <adafetic@gmail.com>
2023-07-13 20:59:40 -0500

    updated jenkinsfile and added pipeline.py updates

--- pipeline.py
+++ pipeline.py
@@ -2,3 +2,12 @@ print('My first python job  within Jenkinsfile.')
 print('My second python job  within Jenkinsfile.')
 print('My third python job  within Jenkinsfile.')
 
+def my_function():
+    print("Hello from a function")
+    
+my_function()
+
+def my_function(fname):
+    print(fname + " Refsnes")
+
+my_function("Emilia")
